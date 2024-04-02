c5b0a1ff49c1a8e07750d8248cdcf2c4041bc384
Ryan Morales <ryan.morales@west-mec.org>
2024-04-02 13:25:09 -0700

    current 12pm

--- server/server.js
+++ server/server.js
@@ -11,3 +11,35 @@ app.get('/', (req, res) => {
 app.listen(port, () => { 
     console.log(`Server is listening on ${port}`);
 });
+
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+// Serve static files from the project root, assuming index.html is there
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    // Send index.html to the client
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.get('/csv-data', (req, res) => {
+    const csvFilePath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading the file');
+            return;
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
