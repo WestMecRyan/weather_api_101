d0486165060f3ecfdb9b60bb3f2d92f2c5ff53c2
Ryan Morales <ryan.morales@west-mec.org>
2024-04-02 00:27:59 +0000

    end of 4/1/24

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => { 
+    res.send('hello world');
+});
+app.get('/3pm', (req, res) => { 
+    res.send('hello 3pm');
+});
+app.listen(port, () => {
+    console.log(`server is listening on port${port}`);
+ })
