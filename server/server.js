408a20792b7e7748dc06dca7c469aae78d2297fc
Ryan Morales <ryan.morales@west-mec.org>
2024-04-03 00:27:51 +0000

    end of day 4/2/24

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors');   
+//const cors = require('cors');   
 const app = express();
-app.use(cors);
+//app.use(cors);
 const port = 3000;
 
 app.get('/', (req, res) => { 
     res.sendFile(path.join(__dirname, '..', 'index.html'));
     //res.send('hello world');
+});
+app.get('/csv-data', (req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if (err) { 
+            console.error(err);
+            return
+        }
+        res.type('text/plain');
+        res.send(data);
+     });
 });
 app.get('/3pm', (req, res) => { 
     res.send('hello 3pm');
