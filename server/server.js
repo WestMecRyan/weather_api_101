24240856428ab8a5e9f4b0f28a63d0ac8e7078f5
Ryan Morales <ryan.morales@west-mec.org>
2024-04-03 21:29:39 +0000

    end of 4/3/24

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-//const cors = require('cors');   
+const cors = require('cors');   
 const app = express();
-//app.use(cors);
+app.use(cors());
 const port = 3000;
 
 app.get('/', (req, res) => { 
